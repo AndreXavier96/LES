ce54128d390cbead3833fc93b1569bfb64fca86a
pedro <a54389@ualg.pt>
2020-03-31 15:01:44 -0700

    forms2

--- LES/utilizadores/forms.py
+++ LES/utilizadores/forms.py
@@ -4,10 +4,17 @@ from django.contrib.auth.models import User
 
 
 class RegisterForm(UserCreationForm):
-    first_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    last_name = forms.CharField(max_length=30, required=False, help_text='Optional.')
-    email = forms.EmailField(max_length=254, help_text='Required. Inform a valid email address.')
+    email = forms.EmailField(max_length=254, required=True, help_text='Required. Inform a valid email address.')
+    tipo_utilizador = forms.CharField(max_length=30, required=False, help_text='Required.')
+    data_nascimento = forms.DateField(required=True, help_text='Required.')
+    n_tele = forms.IntegerField(required=True, help_text='Required.')
+    c_cidadao = forms.IntegerField(required=True, help_text='Required.')
+    deficiencias = forms.BooleanField(required=True, help_text='Required.')
+    localizacao = forms.BooleanField(required=True, help_text='Required.')
+    dados_pessoais = forms.BooleanField(required=True, help_text='Required.')
+    unidade_organica = forms.BooleanField(required=True, help_text='Required.')
 
     class Meta:
         model = User
-        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ('username', 'email', 'tipo_utilizador', 'data_nascimento', 'n_tele', 'c_cidadao', 'deficiencias',
+                  'localizacao', 'dados_pessoais', 'unidade_organica',  'password1', 'password2', )
