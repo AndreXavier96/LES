4d53873c2877591af00565e79c6542b9fdc4b624
pedro <a54389@ualg.pt>
2020-03-31 13:23:17 -0700

    forms2

--- LES/utilizadores/views.py
+++ LES/utilizadores/views.py
@@ -1,12 +1,21 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from .forms import RegisterForm
 
 
-def register(request):
-    form = UserCreationForm()
-    return render(request,
-                  'register.html',
-                  context={"form": form})
+def signup(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect()
+    else:
+        form = SignUpForm()
+    return render(request, 'register.html', {'form': form})
 
 
 def logout(request):
