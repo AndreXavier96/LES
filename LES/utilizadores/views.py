01cb7607eb5ee6a2774949dae0f8f29f4fa20105
pedro <a54389@ualg.pt>
2020-03-31 09:40:22 -0700

    register

--- LES/utilizadores/views.py
+++ LES/utilizadores/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth import logout, authenticate, login
 
-# Create your views here.
+def register(request):
+    form = UserCreationForm
+    return render(request,
+                  "utilizadores/register.html",
+                  context={"form":form})
