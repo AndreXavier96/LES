133e1b9780fa156553428946ba3806e7271e7090
ricardo <a57547@ualg.pt>
2020-03-31 17:49:38 +0100

    logout nas views.py

--- LES/utilizadores/views.py
+++ LES/utilizadores/views.py
@@ -2,11 +2,13 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth import logout, authenticate, login
 
+
 def register(request):
     form = UserCreationForm
     return render(request,
                   "utilizadores/register.html",
-                  context={"form":form})
+                  context={"form": form})
+
 
 def logout_request(request):
     logout(request)
