7ef3591068373f19996c8446b6920c3d7ed8139f
xavi6696 <xavi.6696@gmail.com>
2020-04-17 19:36:36 +0100

    adicionar impor de campus da inscricao

--- LES/utilizadores/models.py
+++ LES/utilizadores/models.py
@@ -1,14 +1,5 @@
 from django.db import models
-
-
-class Campus(models.Model):
-    nome = models.CharField(max_length=255)
-    morada = models.TextField(blank=True, null=True)
-    contacto = models.CharField(max_length=45, blank=True, null=True)
-
-    class Meta:
-        managed = False
-        db_table = 'campus'
+from inscricao.models import Campus
 
 
 class Unidadeorganica(models.Model):
