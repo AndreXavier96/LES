9a8d0851e6ec60328b16e0a1063c67f81cf7bd99
ricardo <a57547@ualg.pt>
2020-04-19 18:13:26 +0100

    Correção do Logout

--- LES/utilizadores/views.py
+++ LES/utilizadores/views.py
@@ -92,7 +92,6 @@ def login_request(request):
             user = authenticate(username=username, password=password)
             if user is not None:
                 login(request, user)
-                messages.info(request, f"You are now logged in as {username}")
                 return redirect('/utilizadores/success')
             else:
                 messages.error(request, "Invalid username or password.")
