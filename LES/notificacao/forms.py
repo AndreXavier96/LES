227f6f924fbe3f3a38d02a5d5efca810d8310828
ricardo <a57547@ualg.pt>
2020-04-21 23:38:17 +0100

    Correção do Register

--- LES/notificacao/forms.py
+++ LES/notificacao/forms.py
@@ -2,9 +2,9 @@ from django import forms
 
 class NotificacaoForm(forms.Form):
     conteudo =forms.CharField(label='conteudo',max_length=200, widget=forms.TextInput({}))
-    hora = forms.DateTimeField(label='hora')
+    #hora = forms.DateTimeField(label='hora')
     prioridade = forms.IntegerField(label='prioridade')
     assunto = forms.CharField(label='assunto',max_length=200, widget=forms.TextInput({}))
     #utilizador_env = forms.IntegerField(label='utilizador_env' )
-    #utilizador_rec = forms.IntegerField(label='utilizador_rec')
+    utilizador_rec =forms.CharField(max_length=200, widget=forms.TextInput({}))
 
--- LES/notificacao/views.py
+++ LES/notificacao/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.utils.datetime_safe import datetime
 from django.views.generic import View
 from .forms import NotificacaoForm
 from .models import Notificacao, Utilizador
@@ -8,8 +9,13 @@ class notificacao(View):
     template_name = 'notificacao.html'
     def get(self, request):
         form = NotificacaoForm()
+        # ---------------get autenticated user
+        auth_user = request.user
+        utilizador_env = Utilizador.objects.get(pk=auth_user.id)
+        # ----------------------
         return render(request, self.template_name, {
                                                     'form': form,
+                                                    'utilizador_env': utilizador_env
                                                     })
     def post(self, request):
         form_notificacao = NotificacaoForm(request.POST)
@@ -18,13 +24,19 @@ class notificacao(View):
             print('asd')
             assunto = form_notificacao['assunto'].value()
             conteudo = form_notificacao['conteudo'].value()
-            hora = form_notificacao['hora'].value()
+            hora = datetime.now()
             prioridade = form_notificacao['prioridade'].value()
             #utilizador_env = form_notificacao['utilizador_env'].value()
-            #utilizador_rec = form_notificacao['utilizador_rec'].value()
-            utilizador_env = Utilizador.objects.get(pk=1)
+            auth_user = request.user
+            utilizador_env = Utilizador.objects.get(pk=auth_user.id)
+            #utilizador_env = Utilizador.objects.get(pk=1)
+            #utilizador_env_value = form_notificacao.cleaned_data.get("utilizador_env")
+            #print(utilizador_env_value)
+            #utilizador_env = Utilizador.objects.get(nome=utilizador_env_value)
+            utilizador_rec= form_notificacao['utilizador_rec'].value()
+            utilizador_rec1 = Utilizador.objects.get(email=utilizador_rec)
             Notificacao.objects.create(assunto=assunto, conteudo=conteudo, hora=hora,
                                        prioridade=prioridade, utilizador_env=utilizador_env,
-                                       utilizador_rec=utilizador_env)
+                                       utilizador_rec=utilizador_rec1)
         return redirect('/notificacao')
 
